0838b25101a01fef4ab14ee8ce7b04fd067c355d
Logan Maxwell <loganmaxwell6@gmailcom>
2021-11-09 10:43:21 +0000

    fix ioc??

--- main.js
+++ main.js
@@ -933,7 +933,7 @@ function determineCipher(text = globalText.slice(0, globalText.length)){
         return "Transposition";
     }
     let i = indexOfCoincidence(text);
-    if (i >= 0.06){
+    if (i >= 0.065 && i <= 0.068){
         return "Substitution"; //placeholder until we narrow down substitution ciphers
     }
     let k = getKeyLength(text)
