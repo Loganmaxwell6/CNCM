29b151fb9956889f0d257bc91f0e59d110c5fb41
Fenwick Thompson <fenwthompson@gmail.com>
2021-12-14 17:32:36 +0000

    light mode added (looks like shit still)

--- main.js
+++ main.js
@@ -199,6 +199,9 @@ function setStyle(style){
         case "biStyle":
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("bi-flag");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(116,77,152,1)');
             root.style.setProperty("--highlightdark", '#5F3F7D');
             break;
@@ -206,6 +209,9 @@ function setStyle(style){
         case "prideStyle":
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("pride-flag");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(0,121,64,1)');
             root.style.setProperty("--highlightdark", 'rgba(1,87,47,1)');
             break;
@@ -213,30 +219,54 @@ function setStyle(style){
         case "panStyle":
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("pan-flag");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(1,148,252,1)');
             root.style.setProperty("--highlightdark", 'rgba(0,87,150,1)');
             break;
         case "transStyle":
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("trans-flag");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(247,168,184,1)');
             root.style.setProperty("--highlightdark", 'rgba(196,133,146,1)');
             break;
-        case 'boringStyle':
+        case 'darkMode':
             document.body.classList.remove(...document.body.classList);
-            document.body.classList.add("boring-style");
+            document.body.classList.add("dark-mode");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', '#666666');
             root.style.setProperty('--highlightdark', '#555555');
+            break;
+        case 'lightMode':
+            document.body.classList.remove(...document.body.classList);
+            document.body.classList.add("light-mode");
+            root.style.setProperty('--bg', 'rgba(255,255,255,1)')
+            root.style.setProperty('--bgdark', 'rgba(180,180,180,1)')
+            root.style.setProperty('--bglight', 'rgba(200,200,200,1)')
+            root.style.setProperty('--highlight', 'rgba(128,110,189,1)');
+            root.style.setProperty('--highlightdark', 'rgba(97,84,143,1)');
             break;
         case 'benStyle':
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("ben-style");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(155,46,144,1)');
             root.style.setProperty('--highlightdark', 'rgba(117,34,109,1)');
             break;
         case 'bingusStyle':
             document.body.classList.remove(...document.body.classList);
             document.body.classList.add("bingus-style");
+            root.style.setProperty('--bg', 'hsl(0, 0%, 25%)')
+            root.style.setProperty('--bgdark', 'hsl(0, 0%, 20%)')
+            root.style.setProperty('--bglight', 'hsl(0, 0%, 30%)')
             root.style.setProperty('--highlight', 'rgba(198,155,154,1)');
             root.style.setProperty('--highlightdark', 'rgba(171,107,105,1)');
             break;
