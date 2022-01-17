c1bba4bd91575f927d5b7356e4a031f54e4e2818
Logan Maxwell <loganmaxwell6@gmailcom>
2022-01-17 09:40:03 +0000

    small stuff

--- phys-sim/main.js
+++ phys-sim/main.js
@@ -21,8 +21,10 @@ function setup(){
   var canvas = createCanvas(MAX_X, MAX_Y);
   canvas.parent("mainCanvas");
   frameRate(fr);
-  for(let i = 10; i < MAX_X - 10; i+=5){
-    render.push(new Obj(i, MAX_Y - i, 20));
+  for(let i = 10; i < MAX_X - 10; i+=10){
+    if(MAX_Y - i >= 10){
+      render.push(new Obj(i, MAX_Y - i, 20));
+    }   
   }
 }
 
