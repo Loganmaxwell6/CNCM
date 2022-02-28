1273048310577c88787a5a03df334fe3f0b81866
Logan Maxwell <loganmaxwell6@gmail.com>
2022-02-28 00:33:01 +0000

    ERROR IS JAVASCRIPT FLOATING POINT !!!!!!!!!

--- phys-sim/main.js
+++ phys-sim/main.js
@@ -1,7 +1,10 @@
 var render = []; //list of all objects on the canvas
-var dt = 0; //timestep - initially 0 to stop simulation by default
+var dt = 0.1; //timestep - initially 0 to stop simulation by default
+var sdt = 0;
 var MAX_X = 1500; //bounds of canvas
 var MAX_Y = 700;
+var MAX_SPEED = 150;
+var MIN_SPEED = dt * 0.1; 
 
 function setup(){ //run once to initialise program
   var canvas = createCanvas(MAX_X, MAX_Y);
@@ -18,15 +21,21 @@ function draw(){ //run on every frame
   for (let i = 0; i < render.length; i++){ //iterate through all objects
     render[i].move(dt); //update each object
   }
+  console.log(render[0]);
 }
 
 function keyPressed(){
-  if(keyCode === RIGHT_ARROW){
+  if(key == "ArrowRight"){
     dt += 0.1;
     dt = Math.round(dt * 10) / 10; //fix javascript floating point decimal stuff
   }
-  if(keyCode === LEFT_ARROW){
+  if(key == "ArrowLeft"){
     dt -= 0.1;
     dt = Math.round(dt * 10) / 10; //fix javascript floating point decimal stuff
+  }
+  if(key == "p"){
+    temp = sdt;
+    sdt = dt;
+    dt = temp;
   }
 }
--- phys-sim/object.js
+++ phys-sim/object.js
@@ -14,6 +14,19 @@ class Particle{
         //update velocity
         this.vx += this.ax * dt;
         this.vy += this.ay * dt;
+        //validate velocity
+        if (this.vx > MAX_SPEED || this.vx < -MAX_SPEED){
+            this.vx = Math.sign(this.vx) * MAX_SPEED;
+        }
+        if (this.vy > MAX_SPEED || this.vy < -MAX_SPEED){
+            this.vy = Math.sign(this.vy) * MAX_SPEED;
+        }
+        if (this.vx < MIN_SPEED && this.vx > -MIN_SPEED){
+            this.vx = 0;
+        }
+        if (this.vy < MIN_SPEED && this.vy > -MIN_SPEED){
+            this.vy = 0;
+        }
         //update position based off velocity
         this.x += this.vx * dt;
         this.y += this.vy * dt;
@@ -24,21 +37,22 @@ class Particle{
     }
 
     checkWallCollisions(){
-        if (this.y + this.size / 2 > MAX_Y){ //bottom wall
-            this.y = MAX_Y - this.size / 2; //reset position to within bounds
+        if(this.y + this.size / 2 > MAX_Y){ // bottom wall
+            let offset = this.y + (this.size / 2) - MAX_Y; //how far the particle has gone past the wall
+            this.y = MAX_Y - this.size / 2 - offset; //set reflected position
             this.vy = -this.vy; //reflect velocity
         }
-        if (this.y - this.size / 2 < 0){ //top wall
-            this.y = this.size / 2; //reset position to within bounds
-            this.vy = -this.vy; //reflect velocity
+        if(this.y <= 0 + this.size / 2){ // top wall
+            this.y = 0 + this.size / 2;
+            this.vy = -this.vy;
         }
-        if (this.x + this.size / 2 > MAX_X){ //right wall
-            this.yx = MAX_X - this.size / 2; //reset position to within bounds
-            this.vx = -this.vx; //reflect velocity
+        if(this.x >= MAX_X - this.size / 2){ // right wall
+            this.x = MAX_X - this.size / 2;
+            this.vx = -this.vx;
         }
-        if (this.x - this.size / 2 < 0){ //left wall
-            this.y = this.size / 2; //reset position to within bounds
-            this.vy = -this.vy; //reflect velocity
+        if(this.x <= 0 + this.size / 2){ // left wall
+            this.x = 0 + this.size / 2;
+            this.vx = -this.vx;
         }
     }
     
