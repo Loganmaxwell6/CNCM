35e21a105a6b428b9b2595a9d3007da43e5f17f3
Logan Maxwell <loganmaxwell6@gmail.com>
2022-02-28 08:05:40 +0000

    stage 1.5 need to do object panel

--- phys-sim/main.js
+++ phys-sim/main.js
@@ -1,41 +1,99 @@
 var render = []; //list of all objects on the canvas
-var dt = 0.1; //timestep - initially 0 to stop simulation by default
+var dt = 0; //timestep - initially 0 to stop simulation by default
 var sdt = 0;
-var MAX_X = 1500; //bounds of canvas
-var MAX_Y = 700;
+var MAX_X = 1100; //bounds of canvas
+var MAX_Y = 550;
 var MAX_SPEED = 150;
 var MIN_SPEED = dt * 0.1; 
+var leftClick = false;
+var rightClick = false;
+var selected = -1;
+var buffer0;
+var buffer1;
+
+var data = {
+  labels: [0],
+  datasets: [{
+    label: "",
+    data: [],
+    fill: false,
+    borderColor: 'rgb(0,0,0)',
+  }]
+};
+
+var objectGraph = new Chart($('#objectGraph'), {
+    type: 'line',
+    data: data,
+});
+
+function addDataPointToGraph(val){
+  let labels = objectGraph.data.labels;
+  objectGraph.data.labels.push(labels[labels.length - 1] + dt);
+  objectGraph.data.datasets[0].data.push(val);
+  objectGraph.update();
+};
+
+function resetGraph(){
+  objectGraph.data = data;
+  objectGraph.update();
+};
+
+function getDtSlider(val){
+  dt = (val - 100) / 100;
+}
 
 function setup(){ //run once to initialise program
   var canvas = createCanvas(MAX_X, MAX_Y);
   canvas.parent("canvasContainer");
   frameRate(60);
+  /*
   for (let i = 0; MAX_Y - i >= 10; i += 20){
     render.push(new Particle((i + 10), (MAX_Y - 10 - i), 20));
   }
-}
+  */
+};
 
 function draw(){ //run on every frame 
   background(200);
-  text(dt.toString(), 10, 30);
+  text(dt.toString(), 10, 25);
+  if(leftClick){
+    if(selected < 0){
+      for(let i = 0; i < render.length; i++){
+        let pos = render[i].getPosVector();
+        let r = render[i].getSize() / 2;
+        if ((((mouseX - pos[0]) ** 2) + ((mouseY - pos[1]) ** 2)) <= (r ** 2)){
+          selected = i;
+          render[i].clear();
+        }
+      }
+    }
+    else{
+      render[selected].clicked(mouseX, mouseY);
+      buffer1 = buffer0;
+      buffer0 = render[selected].getPosVector();
+    }
+  }
   for (let i = 0; i < render.length; i++){ //iterate through all objects
     render[i].move(dt); //update each object
   }
-  console.log(render[0]);
+  //console.log(render[0]);
+};
+
+function mousePressed(){
+  if(mouseButton == LEFT){leftClick = true;}
+  if(mouseButton == RIGHT){
+    rightClick = true;
+    render.push(new Particle(mouseX, mouseY, 30));
+  }
 }
 
-function keyPressed(){
-  if(key == "ArrowRight"){
-    dt += 0.1;
-    dt = Math.round(dt * 10) / 10; //fix javascript floating point decimal stuff
+function mouseReleased(){
+  if(mouseButton == LEFT){
+    leftClick = false;
+    if (selected >= 0){
+      render[selected].deselect(buffer0, buffer1, dt);
+      selected = -1;
+    }
   }
-  if(key == "ArrowLeft"){
-    dt -= 0.1;
-    dt = Math.round(dt * 10) / 10; //fix javascript floating point decimal stuff
-  }
-  if(key == "p"){
-    temp = sdt;
-    sdt = dt;
-    dt = temp;
-  }
+  if(mouseButton == RIGHT){rightClick = false;}
 }
--- phys-sim/object.js
+++ phys-sim/object.js
@@ -8,6 +8,8 @@ class Particle{
         this.ay = 1;
         this.size = size;
         this.colour = color(random(255), random(255), random(255));
+        this.damping = 0.9;
+        this.gravity = true;
     }
 
     move(dt){
@@ -38,21 +40,25 @@ class Particle{
 
     checkWallCollisions(){
         if(this.y + this.size / 2 > MAX_Y){ // bottom wall
+            this.y = MAX_Y - this.size / 2;
+            this.vy = -this.vy * this.damping;
+            /*
             let offset = this.y + (this.size / 2) - MAX_Y; //how far the particle has gone past the wall
             this.y = MAX_Y - this.size / 2 - offset; //set reflected position
             this.vy = -this.vy; //reflect velocity
+            */
         }
         if(this.y <= 0 + this.size / 2){ // top wall
             this.y = 0 + this.size / 2;
-            this.vy = -this.vy;
+            this.vy = -this.vy * this.damping;
         }
         if(this.x >= MAX_X - this.size / 2){ // right wall
             this.x = MAX_X - this.size / 2;
-            this.vx = -this.vx;
+            this.vx = -this.vx * this.damping;
         }
         if(this.x <= 0 + this.size / 2){ // left wall
             this.x = 0 + this.size / 2;
-            this.vx = -this.vx;
+            this.vx = -this.vx * this.damping;
         }
     }
     
@@ -60,5 +66,31 @@ class Particle{
         fill(this.colour);
         stroke(this.colour);
         ellipse(x, y, size, size);
+    }
+
+    clicked(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    clear(){
+        this.vx = 0;
+        this.vy = 0;
+        this.ax = 0;
+        this.ay = 0;
+    }
+
+    deselect(buffer0, buffer1, dt){
+        this.vx = buffer0[0] - buffer1[0];
+        this.vy = buffer0[1] - buffer1[1];
+        if(this.gravity){this.ay = 1;}
+    }
+
+    getPosVector(){
+        return [this.x, this.y];
+    }
+
+    getSize(){
+        return this.size;
     }
 }
